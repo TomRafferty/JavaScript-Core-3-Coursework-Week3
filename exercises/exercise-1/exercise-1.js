86778451f36e89cf1a119fd74417b55ca56453bd
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-11-09 12:05:52 +0000

    exercise 1 complete

--- exercises/exercise-1/exercise-1.js
+++ exercises/exercise-1/exercise-1.js
@@ -3,11 +3,14 @@ const personOne = {
   age: 34,
   favouriteFood: "Spinach",
 };
+//What is the syntax to destructure the object personOne in exercise-1.js?
+//answer:
+let { name, age, favouriteFood } = personOne;
 
-function introduceYourself(___________________________) {
+function introduceYourself(name, age, favouriteFood) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
 }
 
-introduceYourself(personOne);
+introduceYourself(name, age, favouriteFood);
